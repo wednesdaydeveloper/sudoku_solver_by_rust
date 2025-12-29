0eaa667431001decb3f77bf9764fcd019157296d
wednesday.developer <wednesday.developer@gmail.com>
2025-12-29 19:15:26 +0900

    Format Sudoku grid for improved readability in main function

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ pub fn print_grid(grid: &Grid) {
 fn main() {
   // サンプルの数独問題（   Noneは未入力を表す）
   let mut grid: Grid = [
-    [Some(5), Some(3),  None,  None, Some(7),  None,  None,  None,  None],
-    [Some(6),  None,  None, Some(1), Some(9), Some(5),  None,  None,  None],
-    [   None, Some(9), Some(8),  None,  None,  None,  None, Some(6),  None],
-    [Some(8),  None,  None,  None, Some(6),  None,  None,  None, Some(3)],
-    [Some(4),  None,  None, Some(8),  None, Some(3),  None,  None, Some(1)],
-    [Some(7),  None,  None,  None, Some(2),  None,  None,  None, Some(6)],
-    [   None, Some(6),  None,  None,  None,  None, Some(2), Some(8),  None],
-    [   None,  None,  None, Some(4), Some(1), Some(9),  None,  None, Some(5)],
-    [   None,  None,  None,  None, Some(8),  None,  None, Some(7), Some(9)],
+    [Some(5), Some(3),    None,    None, Some(7),    None,    None,    None,    None],
+    [Some(6),    None,    None, Some(1), Some(9), Some(5),    None,    None,    None],
+    [   None, Some(9), Some(8),    None,    None,    None,    None, Some(6),    None],
+    [Some(8),    None,    None,    None, Some(6),    None,    None,    None, Some(3)],
+    [Some(4),    None,    None, Some(8),    None, Some(3),    None,    None, Some(1)],
+    [Some(7),    None,    None,    None, Some(2),    None,    None,    None, Some(6)],
+    [   None, Some(6),    None,    None,    None,    None, Some(2), Some(8),    None],
+    [   None,    None,    None, Some(4), Some(1), Some(9),    None,    None, Some(5)],
+    [   None,    None,    None,    None, Some(8),    None,    None, Some(7), Some(9)],
   ];
   
   print_grid(&grid);
